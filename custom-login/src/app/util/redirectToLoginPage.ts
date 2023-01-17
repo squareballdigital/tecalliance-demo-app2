f39e60507f37722a3ea2a4e63e2457df7574506f
Ana Petrescu <anamaria@squareball.co>
2023-01-17 14:31:52 +0100

    domain change

--- custom-login/src/app/util/redirectToLoginPage.ts
+++ custom-login/src/app/util/redirectToLoginPage.ts
@@ -2,7 +2,7 @@ import sampleConfig from '../app.config';
 const { CLIENT_ID } = process.env;
 
 export default function (languageIndex = localStorage.getItem("lang")) {
-    window.location.href = "https://custom-login.netlify.app"
+    window.location.href = "https://tecalliance-custom-login.netlify.app"
      + "?lang=" + languageIndex
      + "&clientId=" + `${CLIENT_ID}`
      + "&redirectUri=" + sampleConfig.redirectUriAfterLogin;
